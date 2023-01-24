c3a3ab8859ed871ed440a036e28a3cafab6e5fbf
noushinf <83668924+noushinf@users.noreply.github.com>
2023-01-24 13:49:21 +0330

    Finish Serializers

--- blango/settings.py
+++ blango/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'blog',
     'debug_toolbar',
     'blango_auth',
+    'rest_framework',
 
 
 ]
--- blog/api_views.py
+++ blog/api_views.py
@@ -5,27 +5,28 @@ from django.views.decorators.csrf import csrf_exempt
 from blog.models import Post
 import json
 from http import HTTPStatus
+from blog.serializers import PostSerializer
 
 
-def post_to_dict(post):
-    return {
-        "pk": post.pk,
-        "author_id": post.author_id,
-        "created_at": post.created_at,
-        "modified_at": post.modified_at,
-        "published_at": post.published_at,
-        "title": post.title,
-        "slug": post.slug,
-        "summary": post.summary,
-        "content": post.content,
-    }
+# def post_to_dict(post):
+#   return {
+#       "pk": post.pk,
+#       "author_id": post.author_id,
+#       "created_at": post.created_at,
+#       "modified_at": post.modified_at,
+#       "published_at": post.published_at,
+#      "title": post.title,
+#      "slug": post.slug,
+#      "summary": post.summary,
+#      "content": post.content,
+#  }
 
 
 @csrf_exempt
 def post_list(request):
     if request.method == "GET":
         posts = Post.objects.all()
-        posts_as_dict = [post_to_dict(p) for p in posts]
+        posts_as_dict = [PostSerializer(p) for p in posts]
         return JsonResponse({"data": posts_as_dict})
     elif request.method == "POST":
         post_data = json.loads(request.body)
@@ -40,15 +41,18 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "GET":
-        return JsonResponse(post_to_dict(post))
+        return JsonResponse(PostSerializer(post))
     elif request.method == "PUT":
         post_data = json.loads(request.body)
-        for field, value in post_data.items():
-            setattr(post, field, value)
-            post.save()
+        serializer = PostSerializer(post, data=post_data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        # for field, value in post_data.items():
+        # setattr(post, field, value)
+        # post.save()
         return HttpResponse(status=HTTPStatus.NO_CONTENT)
     elif request.method == "DELETE":
         post.delete()
         return HttpResponse(status=HTTPStatus.NO_CONTENT)
     return HttpResponseNotAllowed(["GET", "PUT", "DELETE"])
-
