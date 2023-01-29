4e7ec12a8cc2b01a6f0e87f3a3b106b7a0169879
noushinf <83668924+noushinf@users.noreply.github.com>
2023-01-29 13:00:24 +0330

    "Finish related fields"

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include
 from rest_framework.authtoken import views
 from rest_framework.urlpatterns import format_suffix_patterns
-from api.views import PostList, PostDetail
-#urlpatterns44 = [
- #    path("posts/", PostList.as_view(), name="api_post_list"),
-  #   path("posts/<int:p k>", PostDetail.as_view(), name="api_post_detail"),
-#]
-#urlpatterns = format_suffix_patterns(urlpatterns)
-urlpatterns += [
+from api.views import PostList, PostDetail, UserDetail
+
+urlpatterns = [
    path("auth/", include("rest_framework.urls")),
-   path("token-auth/", views.obtain_auth_token)
+   path("token-auth/", views.obtain_auth_token),
+   path("posts/", PostList.as_view(), name="api_post_list"),
+   path("posts/<int:pk>", PostDetail.as_view(),name="api_post_detail"),
+   path("users/<str:email>", UserDetail.as_view(),name="api_user_detail"),
+
+
 ]
+# urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -1,11 +1,12 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAdminUser
-
+from blango_auth.models import User
 from api.serializers import PostSerializer
 from blog.models import Post
 from api.permissions import AuthorModifyOrReadOnly
 from rest_framework import permissions
 from api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
+from api.serializers import PostSerializer, UserSerializer
 
 
 class PostList(generics.ListCreateAPIView):
@@ -19,3 +20,9 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    lookup_field = "email"
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
